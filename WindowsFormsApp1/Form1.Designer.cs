e3395afae5dddb107d075afec4c9cb6d406b388d
joaochioda <joao.chioda@dextra-sw.com>
2022-04-17 17:31:07 -0300

    auto pot

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkbox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkbox1
+            // 
+            this.checkbox1.AutoSize = true;
+            this.checkbox1.Location = new System.Drawing.Point(59, 47);
+            this.checkbox1.Name = "checkbox1";
+            this.checkbox1.Size = new System.Drawing.Size(57, 19);
+            this.checkbox1.TabIndex = 0;
+            this.checkbox1.Text = "active";
+            this.checkbox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 126);
+            this.Controls.Add(this.checkbox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkbox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Memory;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string sClass, string sWindow);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        Mem m = new Mem();
+        string healthAddress = "AsgardRO.exe+DD1A04";
+        IntPtr MWhandle;
+        IntPtr CWhandle;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int PID = m.GetProcIdFromName("AsgardRO");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                Thread h1 = new Thread(H1) { IsBackground = true };
+                h1.Start();
+            }
+        }
+        bool isGameActive()
+        {
+            MWhandle = (IntPtr)FindWindow(null, "Asgard RO | Gepard Shield 3.0 (^-_-^)");
+            CWhandle = GetForegroundWindow();
+
+            return MWhandle == CWhandle ? true : false;
+        }
+        private void H1()
+        {
+            while (true)
+            {
+                if (checkbox1.Checked && isGameActive())
+                {
+                    int health = m.ReadInt(healthAddress);
+                    if (health < 3000)
+                    {
+                        SendKeys.SendWait("w");
+                        Thread.Sleep(1000);
+                    }
+                    Thread.Sleep(100);
+                }
+                Thread.Sleep(100);
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
